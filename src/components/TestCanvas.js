f15f63b7f5982e7692751c66802d6c359ed0cad0
jalzamora21 <94610731+jalzamora21@users.noreply.github.com>
2023-03-24 19:47:26 +0100

    TestCanvas.js better feedback

--- src/components/TestCanvas.js
+++ src/components/TestCanvas.js
@@ -1,9 +1,26 @@
 import { useRef, useState } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { moveTowards } from '../utils/math';
+import { inverseLerp, moveTowards } from '../utils/math';
+import { Color } from 'three';
 
 const Box = (props) => {
   const speedOnClick = 10;
+  const colorShiftSpeed = 5;
+  const speedStep = 3;
+  const hoverColor = new Color('#0077ff');
+  const defaultColor = new Color('white');
+  const highSpeedColor = new Color('orange');
+  const highSpeedThreshold = 0.7;
+
+  const getTargetColor = (currentSpeed, hovered) => {
+    if (currentSpeed > highSpeedThreshold) {
+      inverseLerp(highSpeedThreshold, speedOnClick, currentSpeed)
+
+      return highSpeedColor.clone().lerp(defaultColor, inverseLerp(speedOnClick, highSpeedThreshold, currentSpeed));
+    }
+    else if (hovered) return hoverColor;
+    else return defaultColor;
+  };
 
   // This reference will give us direct access to the mesh
   const mesh = useRef();
@@ -14,27 +31,34 @@ const Box = (props) => {
   useFrame((state, delta) => {
     const targetSpeed = hovered ? 0.05 : 0.5;
 
-    const speedStep = 3;
-
     const userData = mesh.current.userData;
     userData.currentSpeed ??= targetSpeed;
+    userData.currentColor ??= defaultColor;
     // userData.currentSpeed = clamp(userData.currentSpeed, 0, 0.5);
 
+    const targetColor = getTargetColor(userData.currentSpeed, hovered);
+
+    if (userData.currentSpeed > highSpeedThreshold) userData.currentColor = highSpeedColor;
+
     userData.currentSpeed = moveTowards(userData.currentSpeed, targetSpeed, delta * speedStep);
+    userData.currentColor.lerp(targetColor, delta * colorShiftSpeed);
 
     mesh.current.rotation.x += delta * userData.currentSpeed;
     mesh.current.rotation.y += delta * userData.currentSpeed;
+    mesh.current.material.color = userData.currentColor;
   });
   // Return view, these are regular three.js elements expressed in JSX
   return (
     <mesh
       {...props}
       ref={mesh}
-      scale={active ? 1.5 : 1}
+      scale={active ? 1.1 : 1}
       onClick={(event) => {
         mesh.current.userData.currentSpeed = speedOnClick;
         // setActive(!active);
       }}
+      onPointerDown={() => setActive(true)}
+      onPointerUp={() => setActive(false)}
       onPointerOver={(event) => {
         console.log('pointer over', event);
         setHover(true);
@@ -42,7 +66,7 @@ const Box = (props) => {
       onPointerOut={(event) => setHover(false)}
     >
       <dodecahedronGeometry args={[2]} />
-      <meshStandardMaterial color={hovered ? 'cyan' : 'orange'} wireframe={true} />
+      <meshStandardMaterial wireframe={true} />
     </mesh>
   );
 };
--- src/utils/math.js
+++ src/utils/math.js
@@ -3,3 +3,6 @@ export const moveTowards = (current, target, maxDelta) => {
   else if (current > target) return Math.max(current - maxDelta, target);
   else return current;
 }
+
+const clamp = (a, min = 0, max = 1) => Math.min(max, Math.max(min, a));
+export const inverseLerp = (x, y, a) => clamp((a - x) / (y - x));
