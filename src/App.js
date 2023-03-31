8c57dc7051f26e6b3b0aa0aee699dd14bfe3836b
jalzamora21 <94610731+jalzamora21@users.noreply.github.com>
2023-03-31 19:18:16 +0200

    Debug toggle

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme, CssBaseline, IconButton, ThemeProvider } from '@mui/material';
 import MainPage from './pages/MainPage';
 import { Leva } from 'leva';
 import ScrollNavigation from './components/ScrollNavigation/ScrollNavigation';
+import BugReportIcon from '@mui/icons-material/BugReport';
+import { useState } from 'react';
 
 const darkTheme = createTheme({
   palette: {
@@ -10,9 +12,17 @@ const darkTheme = createTheme({
 });
 
 const App = () => {
+  const [debugActive, setDebugActive] = useState(false);
+
   return (
     <ThemeProvider theme={darkTheme}>
-      <Leva hidden={true} />
+      <IconButton
+        sx={{ position: 'fixed', bottom: 25, right: 25 }}
+        onClick={() => setDebugActive(!debugActive)}
+      >
+        <BugReportIcon color={debugActive ? 'success' : 'inherit'} />
+      </IconButton>
+      <Leva hidden={!debugActive} />
       <CssBaseline />
       <MainPage />
     </ThemeProvider>
